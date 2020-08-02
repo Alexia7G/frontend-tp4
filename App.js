de0b0c6e2670f14e5d535bd3d83fb8762ff25be4
Alexia7G <gallo.alexiaa@hotmail.com>
2020-08-01 22:32:07 -0300

    menu c/iconos - firebase

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navegacion from "./app/navigations/Navegacion";
+import {} from "./app/utils/firebase";
+import * as firebase from "firebase";
 
 export default function App() {
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user);
+    });
+  }, []);
   return <Navegacion />;
 }
--- app/navigations/Navegacion.js
+++ app/navigations/Navegacion.js
@@ -1,19 +1,64 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { Icon } from "react-native-elements";
 import InicioStack from "./InicioStack";
 import FavoritosStack from "./FavoritosStack";
 import CuentaStack from "./CuentaStack";
 
 import Cuenta from "../screens/Cuenta";
 
+const MyTheme = {
+  dark: false,
+  colors: {
+    primary: "rgb(255, 45, 85)",
+    background: "rgb(242, 242, 242)",
+    card: "rgb(255, 255, 255)",
+    text: "rgb(28, 28, 30)",
+    border: "rgb(199, 199, 204)",
+    notification: "rgb(255, 69, 58)",
+  },
+};
+
 const Tab = createBottomTabNavigator();
+
+const screenOptions = (route, color) => {
+  let iconName;
+
+  switch (route.name) {
+    case "inicio":
+      iconName = "home-outline";
+      break;
+    case "mapa":
+      iconName = "map-marker-radius";
+      break;
+    case "cuenta":
+      iconName = "account-circle";
+      break;
+    case "favoritos":
+      iconName = "account-circle-outline";
+      break;
+    default:
+      break;
+  }
+  return (
+    <Icon type="material-community" name={iconName} size={40} color={color} />
+  );
+};
 
 export default function Navegacion() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
+    <NavigationContainer theme={MyTheme}>
+      <Tab.Navigator
+        initialRouteName="inicio"
+        tabBarOptions={{
+          inactiveTintColor: "#646464",
+          activeTintColor: "pink",
+        }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+        })}
+      >
         <Tab.Screen
           name="inicio"
           component={InicioStack}
